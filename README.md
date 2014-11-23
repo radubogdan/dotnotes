3c83080256225ecfd28ce3dad0c3dc5b7613d661
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-11-24 01:14:05 +0200

    first refactoring. add exceptions

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 # Note
 
-TODO: Write a gem description
+A CRUD note library. It will make a directory `.notes` in your HOME.
+You can create, read, update and destroy notes using your terminal.
 
 ## Installation
 
-Add this line to your application's Gemfile:
-
-```ruby
-gem 'note'
-```
-
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
+Install it yourself as:
 
     $ gem install note
 
 ## Usage
 
-TODO: Write usage instructions here
+    note new {filename}    # Create a new note with filename
+    note show {filename}   # Display a note
+    note edit {filename}   # Edit a note
+    note delete {filename} # Delete a note
+    note list              # List all notes
+    note help              # Display this help
+
 
 ## Contributing
 
-1. Fork it ( https://github.com/[my-github-username]/note/fork )
+1. Fork it ( https://github.com/radubogdan/ruby-note/fork )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- lib/note.rb
+++ lib/note.rb
@@ -1,37 +1,96 @@
 require "note/version"
 
+# Don't overwrite default Exceptions but use them
+class NoteAlreadyExist < StandardError; end
+class NoteNotExist < StandardError; end
+
 module Note
   class Application
+
     def initialize
       @notes_path = ENV['HOME'] + '/.notes'
     end
 
-    def init
-      Dir.mkdir @notes_path unless File.exists?(@notes_path)
+    def initialized? p={}
+      path = @notes_path + "/" + p[:file].to_s
+      File.exists?(path)
     end
 
+    # Create a new note in notes path
+    # filename is the name of the note
+    #
+    # @example
+    # begin
+    #   note.new(name)
+    # rescue NoteAlreadyExist
+    #   puts "the note already exist
+    # rescue UninitializedNotesPath
+    #   puts "note.init must be run first"
+    # end
     def new filename
-      file = File.open(@notes_path + "/#{filename}", "w")
-      system("vi #{file.path}")
+      Dir.mkdir @notes_path unless initialized?
+
+      if initialized?(file: filename)
+        raise NoteAlreadyExist
+      else
+        file = File.open(@notes_path + "/#{filename}", "w")
+        system("vi #{file.path}")
+      end
     end
 
+    # Display contents of an existing note
+    # filename is the name of the note
+    #
+    # @example
+    # note.show(name)
     def show filename
       file_path = @notes_path + "/" + filename
-      system("cat #{file_path}") if File.exists?(file_path)
+      system("cat #{file_path}") if initialized?(file: filename)
     end
 
+    # Edit a note
+    # filename is the name of the note
+    #
+    # @example
+    # note.show(name)
     def edit filename
       file_path = @notes_path + "/" + filename
-      system("vi #{file_path}") if File.exists?(file_path)
+      if initialized?(file: filename)
+        system("vi #{file_path}")
+      else
+        raise NoteNotExist
+      end
     end
 
+    # Show all notes
     def list
-      system("stat #{@notes_path}/* -c \"%y %s %n\"")
+      system("stat #{@notes_path}/* -c \"%y %s %n\"") unless Dir[@notes_path + "/*"].empty?
     end
 
+    # Delete a note
+    # filename is the name of the note
+    #
+    # @example
+    # note.delete(name)
     def delete filename
       file_path = @notes_path + "/" + filename
-      system("rm #{file_path}") if File.exists?(file_path)
+      if initialized?(file: filename)
+        system("rm #{file_path}")
+      else
+        raise NoteNotExist
+      end
+    end
+
+    def help
+      puts ""
+      puts "Commands:"
+      puts "  note new {filename}    # Create a new note with filename"
+      puts "  note show {filename}   # Display a note"
+      puts "  note edit {filename}   # Edit a note"
+      puts "  note delete {filename} # Delete a note"
+      puts "  note list              # List all notes"
+      puts "  note help              # Display this help"
+      puts ""
     end
   end
 end
